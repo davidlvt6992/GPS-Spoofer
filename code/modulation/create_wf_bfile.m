1f8b65b83f444a92399a5fc2a9a618c94b4e32fa
דוד לו <levit@campus.technion.ac.il>
2024-02-24 17:48:08 +0200

    [waveform generation] added support for waveform generation

---
+++ code/modulation/create_wf_bfile.m
@@ -0,0 +1,25 @@
+function create_wf_bfile(sv, fs, frames_arr)
+% create_wf_bfile: Creating a binary file containing the waveform
+	% Usage: create_wf_bfile(sv, fs, frames)
+	% Input Args: 
+    %             sv, fs: params for cacode.m which creates the C/A code
+    %             message
+	%             frames_arr: array of frames structs 
+    
+    g = cacode(sv, fs); % creating C/A code matrix
+    
+    % creating frames bitstream matrix
+    fr_bitstream_matrix = [];
+    for i=1:length(frames_arr)
+        fr_bitstream_matrix = [fr_bitstream_matrix ; frames_arr(i).bit_stream];
+    end
+    
+    [fid, message] = fopen("waveform.bin", 'w');
+    if fid < 0 
+        disp(message) 
+    else
+        wf = get_waveform(g, fr_bitstream_matrix);
+        fwrite (fid, wf);
+        fclose(fid);
+    end
+end
