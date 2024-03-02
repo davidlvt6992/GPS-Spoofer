68a1fad9dd68c0e35ae5fee9b8cd4396c3bc136f
Yuval Kurin <yuvalku100@gmail.com>
2024-03-02 15:41:04 +0200

    changes in waveform functions functionallity, added bitstream to analog conversion

--- code/modulation/create_wf_bfile.m
+++ code/modulation/create_wf_bfile.m
@@ -6,22 +6,46 @@ function create_wf_bfile(sv, fs, frames_arr)
     %             message
 	%             frames_arr: array of frames structs 
     
-    g = cacode(sv, fs); % creating C/A code matrix
+    g = cacode(sv, fs); % creating C/A code array
     
-    % creating frames bitstream matrix
-    fr_bitstream_matrix = [];
+    % concat frames bitstream (for the same sattelite)
+    fr_bitstream = [];
     for i=1:length(frames_arr)
-        fr_bitstream_matrix = [fr_bitstream_matrix ; frames_arr(i).bit_stream];
+        fr_bitstream = [fr_bitstream frames_arr(i).bit_stream];
     end
     
-    [fid, message] = fopen("waveform.bin", 'w');
+    [fid, message] = fopen("./shared_folder/attempt1/waveform.bin","w"); %insert path to write
+    
     if fid < 0 
         disp(message) 
     else
-        wf = get_waveform(g, fr_bitstream_matrix);
-        fwrite (fid, wf);
-        message = sprintf('Waveform binary file was created in:\n%s',pwd);
+        wf = get_waveform(g, fr_bitstream);
+
+
+% %TEST SECTION
+%         a = 0.5<rand(1,4500);
+%         ca = cacode(5,4);
+%         ca = repmat(ca,1,20);
+%         
+%         
+%         wf = zeros(1,length(ca)*length(a));
+%         for i = 1:length(a)
+%             start_ind = (i-1)*length(ca)+1;
+%             end_ind = start_ind+length(ca)-1;
+%             wf(start_ind:end_ind) = xor(ca,a(i));
+%         end
+% %END TEST SECTION
+
+
+
+
+        wf = upsample(wf,2); %upsample to accommodate Q channel signal (which is 0)
+        wf = (wf-1/2).*(2^15); %convert wf to analog values
+%         figure()
+%         plot(wf(1:1000))
+        fwrite (fid, wf,"int16");
+%         message = sprintf('Waveform binary file was created in:\n%s',pwd);
         disp(message)
         fclose(fid);
-    end
+    end 
 end
--- code/modulation/test_waveform.m
+++ code/modulation/test_waveform.m
@@ -8,7 +8,7 @@ eph2 = eph_formatted_{4}; %ephemeris data
 
 %create structure for additional parameters
 others_struct = struct;
-others_struct.TLM_msg = zeros(1,14); %TLM message, used by authorised user.
+others_struct.TLM_msg = 0.5<rand(1,14); %TLM message, used by authorised user.
 others_struct.TOW = 15000*6; %time of week in seconds
 others_struct.alert_flag = 0; %URA alert flag
 others_struct.AS_flag = 0; %anti spoof flag
@@ -21,8 +21,8 @@ others_struct.T_GD = 2^-31+2^-27; %group delay time
 others_struct.fit_flag = 0;
 others_struct.AODO = [0 0 0 0 0]; %age of data offset
 
-fr_arr = [frame(eph1, others_struct) frame(eph2, others_struct)];
+fr_arr = [frame(eph1, others_struct) frame(eph1, others_struct) frame(eph1, others_struct)];
 
 %% creating new wf_bfile 
 % we will use C/A code of a single SV and sampling of 4
-create_wf_bfile([1], 4, fr_arr);
+create_wf_bfile([16], 4, fr_arr);
