cb2480f4cef20aedd1c25d479d3127d18f1b2657
דוד לו <levit@campus.technion.ac.il>
2024-03-11 17:50:16 +0200

    Added comments and documentation for the function

--- code/utility/get_combined_waveform.m
+++ code/utility/get_combined_waveform.m
@@ -3,20 +3,31 @@ function combined_waveform = get_combined_waveform(waveform_matrix, pseudo_range
 % in the following eq : pr_i = d(user_location, SV_i_locaction(t-tau))- DSV_i(t-tau)*C
 	% Usage: pr_vec = get_pseudo_ranges(t, eph, x)
 	% Input Args: 
-    %             t: Time of recievd signals (need to think if UTC or GPS
-	% time) 
-	%             eph: Ephemeris matrix 
-	%             x: User location
+    %             waveform_matrix: each row is a SV's waveform
+	%             pseudo_ranges_arr: pseudo ranges array foreach SV
+	%             samp_freq: Sampling frequency of the data
+    
+    % constants defenitions 
     c = 299792458;
     waveform_num = size(waveform_matrix, 1);
     base_size = size(waveform_matrix, 2);
     
-    pseudo_ranges_arr = pseudo_ranges_arr / c;
-    shift_arr = ceil(pseudo_ranges_arr * samp_freq);
+    % getting amount of shifts for each bitstream 
+    shift_arr = ceil(pseudo_ranges_arr / c * samp_freq);
+    
+    % getting maximum amount of shifts that is required
     max_shift = max(shift_arr);
+    
+    % creating the new_waveform matrix of dim [waveform_num, base_size+max_shift]
     new_waveform_matrix = zeros(waveform_num, max_shift + base_size);
+    
+    % for each bitstream , we will make a new row in new_waveform_matrix
+    % which has the appropriate shift, then the data and then the shifts
+    % required to complete the dimensions 
     for i=1:waveform_num
         new_waveform_matrix(i,:) = [zeros(1, shift_arr(i)) waveform_matrix(i,:) zeros(1, max_shift - shift_arr(i))];
     end
-    combined_waveform = sum(waveform_matrix, 1);
+    
+    % the combined_waveform would be the summation of all rows in new_waveform_matrix
+    combined_waveform = sum(new_waveform_matrix, 1);
 
