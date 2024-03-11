6b92a2574cec03a0587ca0e82b4858cb813bc80e
דוד לו <levit@campus.technion.ac.il>
2024-03-11 17:51:51 +0200

    Added comments and documentation for the function

--- code/utility/get_combined_waveform.m
+++ code/utility/get_combined_waveform.m
@@ -1,7 +1,7 @@
 function combined_waveform = get_combined_waveform(waveform_matrix, pseudo_ranges_arr, samp_freq)
-% get_pseudo_ranges: Getting pseudo ranges biased by SV biases as described
-% in the following eq : pr_i = d(user_location, SV_i_locaction(t-tau))- DSV_i(t-tau)*C
-	% Usage: pr_vec = get_pseudo_ranges(t, eph, x)
+% get_combined_waveform: Creating a single waveform broadcast from several
+% SVs waveforms
+	% Usage: combined_waveform = get_combined_waveform(waveform_matrix, pseudo_ranges_arr, samp_freq)
 	% Input Args: 
     %             waveform_matrix: each row is a SV's waveform
 	%             pseudo_ranges_arr: pseudo ranges array foreach SV
