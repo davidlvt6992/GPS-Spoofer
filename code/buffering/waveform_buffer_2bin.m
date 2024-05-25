f73a59168e58fe5702aeae69492415a179607a61
Yuval Kurin <yuvalku100@gmail.com>
2024-05-26 00:19:47 +0300

    added function waveform_buffer_2bin which dumps buffer straight to binary file.

--- code/buffering/waveform_buffer_2bin.m
+++ code/buffering/waveform_buffer_2bin.m
@@ -1,7 +1,7 @@
-function waveform = waveform_buffer(sv_num,sv_bitstreams,LLA_pos,RX_TOW,eph_mat,...
-    f_samp,f_dop,buffer_size)
-%waveform_buffer creates a combined modulated waveform of several SVs using
-%a buffer.
+function waveform_buffer_2bin(sv_num,sv_bitstreams,LLA_pos,RX_TOW,eph_mat,...
+    f_samp,f_dop,buffer_size,path)
+%waveform_buffer_2bin creates a combined modulated waveform of several SVs using
+%a buffer and dumps it directly to a bin file.
 %   inputs: sv_num (nX1) - SV id vector
 %   sv_bitstreams nX(bitstream length) - bit stream matrix of n SVs
 %   LLA_pos - user position in LLA coordinates
@@ -10,6 +10,7 @@ function waveform = waveform_buffer(sv_num,sv_bitstreams,LLA_pos,RX_TOW,eph_mat,
 %   f_samp - sampling frequency (scalar)
 %   f_dop - doppler frequencied vector (nX1)
 %   buffer_size - size of buffer (number of cells in the array)
+%   path - path to binary file
 
 %some constants:
 f_bit = 50; %bits per second
@@ -36,12 +37,26 @@ PR = zeros(n,1); %pseudo-range vector
 iter_num = 1;
 expected_iter = expected_size/buffer_size;
 
+%open file
+[fid, message] = fopen(path,"w"); %insert path to write
+
+if fid < 0
+    disp(message)
+end
+
+
 while (t_j<T)
     PR = get_pseudo_ranges_itr(t_i+RX_TOW,eph_mat,ECEF_pos); %calculate PR for current interval
     buffer = get_combined_wf_interval(t_i,buffer_size,sv_num,PR,sv_bitstreams,...
         f_samp,f_dop); %fill buffer with current interval
     buffer = awgn(buffer,-40); %add white noise to signal
-    waveform_aux(i:j) = buffer; %fill auxilary array with buffer at relevant interval
+%     waveform_aux(i:j) = buffer; %fill auxilary array with buffer at relevant interval
+    buffer_real = real(buffer);
+    buffer_imag = imag(buffer);
+    stacked = [buffer_real;buffer_imag];
+    interleaved_buffer = stacked(:)';
+    fwrite(fid,interleaved_buffer,'int16'); %write to file
+%     fwrite(fid,)
 
     %progress massage
     prog = 100*iter_num/expected_iter;
@@ -56,7 +71,7 @@ while (t_j<T)
     iter_num = iter_num+1;
 end
 
-waveform=waveform_aux;
+fclose(fid);
 
 end
 
--- code/tests/buffer_tests/waveform_buffer_test.m
+++ code/tests/buffer_tests/waveform_buffer_test.m
@@ -16,52 +16,64 @@ btsr30 = [];
 btsr16 = [];
 btsr21 = [];
 btsr25 = [];
+btsr29 = [];
+btsr31 = [];
 
-eph_mat = eph_formatted_([7,3,4,5]);
+eph_mat = eph_formatted_([7,3,4,5,6,8]);
 %create 3*1500 frames bit-streams
 for i=1:3
     fr30 = frame(eph_formatted_{7},others_struct); %sv 30
     fr16 = frame(eph_formatted_{3},others_struct); %sv 16
     fr21 = frame(eph_formatted_{4},others_struct); %sv 21
     fr25 = frame(eph_formatted_{5},others_struct); %sv 25
+    fr29 = frame(eph_formatted_{6},others_struct); %sv 29
+    fr31 = frame(eph_formatted_{8},others_struct); %sv 31
     btsr30 = [btsr30 fr30.bit_stream];
     btsr16 = [btsr16 fr16.bit_stream];
     btsr21 = [btsr21 fr21.bit_stream];
     btsr25 = [btsr25 fr25.bit_stream];
+    btsr29 = [btsr29 fr29.bit_stream];
+    btsr31 = [btsr31 fr31.bit_stream];
+
     others_struct.TOW = others_struct.TOW+30;
 end
 
-sv_num = [30;16;21;25];
-sv_bitsreams = [btsr30 ; btsr16 ; btsr21 ; btsr25];
-clear dig_wf30 dig_wf16 dig_wf21 dig_wf25;
+sv_num = [30;16;21;25;29;31];
+sv_bitsreams = [btsr30 ; btsr16 ; btsr21 ; btsr25 ; btsr29 ; btsr31];
+clear btsr30 btsr16 btsr21 btsr25 btsr29 btsr31;
 
 madrid_lla = [40.41524063127617, -3.6825268152573467 0];
 RX_TOW = rcvr_tow;
 
-f_dop = [2e3;-4e3;6e3;-5e3]; %doppler frequencies
-buffer_size = modulated_bit_length;
+f_dop = [2e3;-4e3;6e3;-5e3;1e3;4e3]; %doppler frequencies
+buffer_size = 2*modulated_bit_length;
 
-waveform = waveform_buffer(sv_num,sv_bitsreams,madrid_lla,RX_TOW,eph_mat,...
-    f_samp,f_dop,buffer_size); %modulated combined waveform.
+path = "GNSS_files\GNSS_waveforms\waveform_buffer_2bin_6sv.bin";
 
+waveform_buffer_2bin(sv_num,sv_bitsreams,madrid_lla,RX_TOW,eph_mat,...
+    f_samp,f_dop,buffer_size,path); %modulated combined waveform.
 
-wf_real = real(waveform);
-wf_imag = imag(waveform);
-clear waveform;
-stacked = [wf_real;wf_imag];
-clear wf_real wf_imag;
-interleaved_wf = stacked(:)';
-clear stacked;
-
-[fid, message] = fopen("GNSS_files\GNSS_waveforms\waveform_buffer.bin","w"); %insert path to write
-    
-if fid < 0
-    disp(message)
-end
-
-fwrite (fid, interleaved_wf,"int16");
-
-fclose(fid);
-% clear;
+% waveform = waveform_buffer(sv_num,sv_bitsreams,madrid_lla,RX_TOW,eph_mat,...
+%     f_samp,f_dop,buffer_size); %modulated combined waveform.
+% 
+% 
+% wf_real = real(waveform);
+% wf_imag = imag(waveform);
+% clear waveform;
+% stacked = [wf_real;wf_imag];
+% clear wf_real wf_imag;
+% interleaved_wf = stacked(:)';
+% clear stacked;
+% 
+% [fid, message] = fopen("GNSS_files\GNSS_waveforms\waveform_buffer.bin","w"); %insert path to write
+%     
+% if fid < 0
+%     disp(message)
+% end
+% 
+% fwrite (fid, interleaved_wf,"int16");
+% 
+% fclose(fid);
+% % clear;
 
 
